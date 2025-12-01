d23c1519f173789a99d9f9cf8ea50013086b1a01
robkam <robkam@gmx.com>
2025-12-01 16:18:15 +0000

    fix: update stats on tagging in file window and default to comma separator

--- init.c
+++ init.c
@@ -87,7 +87,12 @@ int Init(char *configuration_file, char *history_file)
 
   SetFileMode( strtol(FILEMODE, NULL, 0) );
   SetKindOfSort( SORT_BY_NAME );
-  number_seperator = *(NUMBERSEP);
+  char *sep_str = NUMBERSEP;
+  if (sep_str && *sep_str) {
+      number_seperator = *sep_str;
+  } else {
+      number_seperator = ','; /* Default to English style */
+  }
   bypass_small_window = (strtol(NOSMALLWINDOW, NULL, 0 )) ? TRUE : FALSE;
   highlight_full_line = (strtol(GetProfileValue("HIGHLIGHT_FULL_LINE"), NULL, 0)) ? TRUE : FALSE;
   hide_dot_files = (strtol(HIDEDOTFILES, NULL, 0)) ? TRUE : FALSE;
