8b211233feed9ee4ae9060833297e14c7ba64bcf
robkam <robkam@gmx.com>
2026-06-27 15:14:32 +0100

    refactor(appstate): route message state through helper

---
+++ include/ytnova_appstate_message.h
@@ -0,0 +1,16 @@
+/***************************************************************************
+ *
+ * ytnova_appstate_message.h
+ * Message-state transition commits for AppState boundaries.
+ *
+ ***************************************************************************/
+
+#ifndef YTNOVA_APPSTATE_MESSAGE_H
+#define YTNOVA_APPSTATE_MESSAGE_H
+
+#include "ytnova_defs.h"
+
+BOOL AppStateCommitStatusLineError(ViewContext *ctx, const char *message);
+BOOL AppStateClearStatusLineError(ViewContext *ctx);
+
+#endif /* YTNOVA_APPSTATE_MESSAGE_H */
